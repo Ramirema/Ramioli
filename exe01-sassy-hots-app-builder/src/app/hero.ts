6f09401270edeb5cb0f533cc1fface5c00a5dbda
delvauxo <delvauxo@outlook.com>
2018-02-21 10:08:02 +0100

    WEB APP START

---
+++ exe01-sassy-hots-app-builder/src/app/hero.ts
@@ -0,0 +1,10 @@
+export class Hero {
+  name: string;
+  short_name: string;
+  attribute_id: string;
+  role: string;
+  type: string;
+  icon_url: { '92x93': string};
+  talents: string[];
+
+}
---
+++ exe01-sassy-hots-app-builder/src/app/heroes/heroes.component.ts
@@ -0,0 +1,48 @@
+// CONTROLLER
+// --------------------
+// MAIN
+import { Component, OnInit, NgModule } from '@angular/core';
+// MODEL -- TYPES
+import { Hero } from '../hero';
+// JSON -- DATA
+// import { HEROES } from '../mock-heroes';
+// SERVICE
+import { HeroService } from '../hero.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+}
