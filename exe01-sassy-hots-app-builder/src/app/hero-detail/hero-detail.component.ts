917ab85ebd62f64660ea440c202c0879e9a04109
delvauxo <delvauxo@outlook.com>
2018-02-27 16:27:58 +0100

    HERO-DETAILS : ON PROGRESS [TALENTS DISPLAY WITH GOOD URL]

--- exe01-sassy-hots-app-builder/src/app/hero-detail/hero-detail.component.ts
+++ exe01-sassy-hots-app-builder/src/app/hero-detail/hero-detail.component.ts
@@ -18,28 +18,30 @@ export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
   public heroz;
   public abilities;
+  public talents;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
     this.getHeroLinkName();
     this.getHeroAbilities();
+    this.getHeroTalents();
   }
 
   getHero(): void {
     const short_name = this.route.snapshot.paramMap.get('short_name');
     this.heroService.getHero(short_name)
-    .subscribe(
-      hero => {
-        this.hero = hero;
-        // console.log(hero);
-      }
-    );
+      .subscribe(
+        hero => {
+          this.hero = hero;
+          // console.log(hero);
+        }
+      );
 
   }
 
@@ -47,39 +49,70 @@ export class HeroDetailComponent implements OnInit {
     const short_name = this.route.snapshot.paramMap.get('short_name');
 
     this.heroService.getHero(short_name)
-    .subscribe(
-      data => {
+      .subscribe(
+        data => {
 
-        // Rajoute la propriété SORT pour ordonner
-        this.abilities = data.abilities;
-        this.abilities[0].sort = 6;
-        this.abilities[1].sort = 3;
-        this.abilities[2].sort = 1;
-        this.abilities[3].sort = 4;
-        this.abilities[4].sort = 5;
-        this.abilities[5].sort = 2;
+          // Rajoute la propriété SORT pour ordonner
+          this.abilities = data.abilities;
+          this.abilities[0].sort = 6;
+          this.abilities[1].sort = 3;
+          this.abilities[2].sort = 1;
+          this.abilities[3].sort = 4;
+          this.abilities[4].sort = 5;
+          this.abilities[5].sort = 2;
 
-        // Liste les abilities NAME par ordre ASC
-        this.abilities = this.abilities
-        .sort((a, b) => {
-          if (a.sort < b.sort) { return -1; }
-          if (a.sort > b.sort) { return 1; }
-          return 0;
-        });
+          // Liste les abilities SORT par ordre ASC
+          this.abilities = this.abilities
+            .sort((a, b) => {
+              if (a.sort < b.sort) { return -1; }
+              if (a.sort > b.sort) { return 1; }
+              return 0;
+            });
 
-        // Format all abilities names as formatted link name
-        for (let index = 0; index < data.abilities.length; index++) {
-          this.abilities[index].title = data.abilities[index].title
-          .toLowerCase()
-          .replace(/[\'.]/g, '')
-          .replace(/[ ]/g, '-')
-          .replace('ú', 'u');
+          // Format all abilities names as formatted link name
+          for (let index = 0; index < data.abilities.length; index++) {
+            this.abilities[index].title = data.abilities[index].title
+              .toLowerCase()
+              .replace(/[\'.]/g, '')
+              .replace(/[ ]/g, '-')
+              .replace('ú', 'u');
+          }
+
+          // console.log(data.abilities);
+
         }
+      );
+  }
 
-        // console.log(data.abilities);
+  getHeroTalents(): void {
+    const short_name = this.route.snapshot.paramMap.get('short_name');
+    this.heroService.getHeroLinkName(short_name)
+      .subscribe(
+        data => {
 
-      }
-    );
+          // Liste les abilities SORT par ordre ASC
+          this.talents = data.talents
+            .sort((a, b) => {
+              const n = a.level - b.level;
+              if (n !== 0) {
+                return n;
+              }
+              return a.sort - b.sort;
+              // if (a.level < b.level) { return -1; }
+              // if (a.level > b.level) { return 1; }
+              // return 0;
+            });
+            console.log(this.talents);
+          // Format all talents names as formatted link name
+          for (let index = 0; index < data.talents.length; index++) {
+            this.talents[index].title = data.talents[index].title
+              // .toLowerCase()
+              .replace(/[\'.]/g, '')
+              .replace(/[ ]/g, '')
+              .replace('ú', 'u');
+          }
+        }
+      );
   }
 
   getHeroLinkName(): void {
@@ -88,10 +121,10 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(
         data => {
           const heroLinkName = data.name
-          .toLowerCase()
-          .replace(/[\'.]/g, '')
-          .replace(/[ ]/g, '-')
-          .replace('ú', 'u');
+            .toLowerCase()
+            .replace(/[\'.]/g, '')
+            .replace(/[ ]/g, '-')
+            .replace('ú', 'u');
           // console.log(heroLinkName);
           return this.heroz = heroLinkName;
         }
--- exe01-sassy-hots-app-builder/src/app/hero.ts
+++ exe01-sassy-hots-app-builder/src/app/hero.ts
@@ -1,11 +1,25 @@
 export class Hero {
+  title: any;
   name: string;
   short_name: string;
   attribute_id: string;
   role: string;
   type: string;
-  icon_url: { '92x93': string};
-  talents: any[];
+  icon_url: { '92x93': string };
+  talents: [{
+    'name': string;
+    'title': string;
+    'description': string;
+    'icon': string;
+    'icon_url': {
+      '64x64': string;
+    },
+    'ability': string;
+    'sort': number;
+    'cooldown': any;
+    'mana_cost': any;
+    'level': number
+  }];
   abilities: any[];
   translations: any[];
   release_date: Date;
