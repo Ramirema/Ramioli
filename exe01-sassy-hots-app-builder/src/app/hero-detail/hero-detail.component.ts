e3af10218c314c856ce5018a4299fc3e5068e601
delvauxo <delvauxo@outlook.com>
2018-03-02 16:46:44 +0100

    HERO-DETAILS : ABILITIES PICS FIXED [NEED TO SORT ABA + NOVA]

--- exe01-sassy-hots-app-builder/src/app/hero-detail/hero-detail.component.ts
+++ exe01-sassy-hots-app-builder/src/app/hero-detail/hero-detail.component.ts
@@ -52,43 +52,65 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(
         data => {
 
-          // Rajoute la propriété SORT pour ordonner
           this.abilities = data.abilities;
 
+          // Rajoute la propriété SORT pour ordonner
           for (let z = 0; z < data.abilities.length; z++) {
 
-            // #1 ABILITIES
+            // #1 BASIC ABILITIES
             if (this.abilities[z].name === 'Q1') {
               this.abilities[z].sort = 1;
             } else if (this.abilities[z].name === 'W1') {
               this.abilities[z].sort = 2;
             } else if (this.abilities[z].name === 'E1') {
               this.abilities[z].sort = 3;
+            // HEROIC ABILITIES
             } else if (this.abilities[z].name === 'R1') {
               this.abilities[z].sort = 4;
             } else if (this.abilities[z].name === 'R2') {
               this.abilities[z].sort = 5;
             } else if (this.abilities[z].name === 'R3') {
               this.abilities[z].sort = 6;
-              // TRAIT
+              // TRAITS
             } else if (this.abilities[z].name === 'D1') {
               this.abilities[z].sort = 7;
               this.abilities[z].trait = 'Trait';
-            // #2 ABILITIES
-            } else if (this.abilities[z].name === 'Q2') {
-              this.abilities[z].sort = 8;
-            } else if (this.abilities[z].name === 'W2') {
-              this.abilities[z].sort = 9;
-            } else if (this.abilities[z].name === 'E2') {
-              this.abilities[z].sort = 10;
             } else if (this.abilities[z].name === 'D2') {
+              this.abilities[z].sort = 8;
+              this.abilities[z].trait = 'Trait';
+            } else if (this.abilities[z].name === 'D3') {
+              this.abilities[z].sort = 9;
+              this.abilities[z].trait = 'Trait';
+            // #2 SECONDARY ABILITIES
+            } else if (this.abilities[z].name === 'Q2') {
+              this.abilities[z].sort = 10;
+            } else if (this.abilities[z].name === 'W2') {
               this.abilities[z].sort = 11;
-            // BASELINE ABILITY
-            } else if (this.abilities[z].hotkey === '1') {
+            } else if (this.abilities[z].name === 'E2') {
               this.abilities[z].sort = 12;
+            } else if (this.abilities[z].name === 'D2') {
+              this.abilities[z].sort = 13;
+            // BASELINE ABILITIES
+            } else if (this.abilities[z].hotkey === '1') {
+              this.abilities[z].sort = 14;
             // OTHERS
             } else {
-              this.abilities[z].sort = z + 12;
+              this.abilities[z].sort = z + 14;
+            }
+
+            // ABATHUR [ALL] ICONS FIXED
+            if (this.abilities[z].title === 'Carapace') {
+              this.abilities[z].sort = 12;
+              this.abilities[z].name = 'E2';
+              this.abilities[z].icon = '../../assets/icons/abilities/abathur/carapace.png';
+            } else if (this.abilities[z].title === 'Stab') {
+              this.abilities[z].icon = '../../assets/icons/abilities/abathur/stab.png';
+            } else if (this.abilities[z].title === 'Spike Burst') {
+              this.abilities[z].icon = '../../assets/icons/abilities/abathur/spike-burst.png';
+            } else if (this.abilities[z].title === 'Deep Tunnel') {
+              this.abilities[z].icon = '../../assets/icons/abilities/abathur/deep-tunnel.png';
+            } else if (this.abilities[z].name === 'R3') {
+              this.abilities.splice(z, 1);
             }
 
             // BRIGHTWING [MOUNT] ICON FIXED
@@ -101,9 +123,100 @@ export class HeroDetailComponent implements OnInit {
               this.abilities[z].icon = '../../assets/icons/abilities/dehaka/brushstalker.png';
             }
 
+            // FALSTAF [MOUNT] ICON FIXED
+            if (this.abilities[z].title === 'Flight') {
+              this.abilities[z].icon = '../../assets/icons/abilities/falstad/flight.png';
+            }
+
+            // GALL [BASELINE ABILITY - EYE OF KILLROGG] ICON FIXED
+            if (this.abilities[z].title === 'Eye of Kilrogg') {
+              this.abilities[z].icon = '../../assets/icons/abilities/gall/eye-of-killrogg.png';
+            }
+
+            // GAZLOWE [BASELINE ABILITY - ROCKIT TURRET] ICON FIXED
+            if (this.abilities[z].title === 'Focus Turrets!') {
+              this.abilities[z].icon = '../../assets/icons/abilities/gazlowe/rockit-turret.png';
+            }
+
+            // KHARAZIM [ALL TRAITS] ICONS FIXED
+            if (this.abilities[z].title === 'Insight') {
+              this.abilities[z].icon = '../../assets/icons/abilities/kharazim/insight.png';
+            } else if (this.abilities[z].title === 'Transcendence') {
+              this.abilities[z].icon = '../../assets/icons/abilities/kharazim/transcendence.png';
+            } else if (this.abilities[z].title === 'Iron Fists') {
+              this.abilities[z].icon = '../../assets/icons/abilities/kharazim/iron-fists.png';
+            }
+
+            // LEORIC [Drain essence + Ghastly Swing + Wrath Of The Bone King ] ICONS FIXED
+            if (this.abilities[z].title === 'Drain Essence') {
+              this.abilities[z].icon = '../../assets/icons/abilities/leoric/drain-essence.png';
+            } else if (this.abilities[z].title === 'Wrath Of The Bone King') {
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/leoric_trait.png';
+            } else if (this.abilities[z].title === 'Ghastly Swing') {
+              this.abilities[z].icon = '../../assets/icons/abilities/leoric/ghastly-swing.png';
+            }
+
+            // LUNARA [MOUNT] ICONS FIXED
+            if (this.abilities[z].title === 'Dryad\'s Swiftness') {
+              this.abilities[z].icon = '../../assets/icons/abilities/lunara/dryads-swiftness.png';
+            }
+
+            // NOVA [TRAIT + BASELINE ABILITIES] ICONS FIXED
+            if (this.abilities[z].title === 'Permanent Cloak') {
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/nova_personalcloaking.png';
+            } else if (this.abilities[z].title === 'Ghost Protocol') {
+              this.abilities[z].icon = '../../assets/icons/abilities/nova/ghost-protocol.png';
+            } else if (this.abilities[z].title === 'Sniper') {
+              console.log(z); // 2
+              this.abilities.splice(z, 1);
+            }
+
+            // PROBIUS [MOUNT] ICON FIXED
+            if (this.abilities[z].title === 'Worker Rush') {
+              this.abilities[z].icon = '../../assets/icons/abilities/probius/worker-rush.png';
+            }
+
+            // SAMURO [TRAIT] ICON FIXED
+            if (this.abilities[z].title === 'Image Transmission') {
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/samuro_flowingstrikes.png';
+            }
+
+            // HAMMER [Neosteel Plating] ICON FIXED
+            if (this.abilities[z].title === 'Neosteel Plating') {
+              // tslint:disable-next-line:max-line-length
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/sgthammer_neosteelplating.png';
+            }
+
+            // STITCHES [BASELINE ABILITY] ICON FIXED
+            if (this.abilities[z].title === 'Helping Hand') {
+              // tslint:disable-next-line:max-line-length
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/stitches_hook_var1.png';
+            }
+
+            // THE LOST VIKINGS [ALL] ICONS FIXED
+            if (this.abilities[z].title === 'Select Olaf') {
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/lostvikings_selectolaf.png';
+            } else if (this.abilities[z].title === 'Select Baleog') {
+              // tslint:disable-next-line:max-line-length
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/lostvikings_selectbaleog.png';
+            } else if (this.abilities[z].title === 'Select Erik') {
+              this.abilities[z].icon = 'https://psionic-storm.com/wp-content/themes/psionicstorm/img/abilities/lostvikings_selecterik.png';
+            } else if (this.abilities[z].title === 'Viking Bribery') {
+              this.abilities[z].icon = '../../assets/icons/abilities/vikings/viking-bribery.png';
+            } else if (this.abilities[z].title === 'Select All Vikings') {
+              this.abilities[z].icon = '../../assets/icons/abilities/vikings/select-all.png';
+            } else if (this.abilities[z].title === 'Go Go Go!') {
+              this.abilities[z].icon = '../../assets/icons/abilities/vikings/gogogo.png';
+            } else if (this.abilities[z].title === 'Nordic Attack Squad') {
+              this.abilities[z].icon = '../../assets/icons/abilities/vikings/nordic-attack-squad.png';
+            }
+
             // TYCHUS [ODIN ABILITIES] ICONS FIXED
             if (this.abilities[z].title === 'Ragnarok Missiles') {
               this.abilities[z].icon = '../../assets/icons/abilities/tychus/odin/tychus-odin-ragnarok-rissiles.png';
+            // HAMMER SAME TYCHUS THRUSTERS ABILITY NAME
+            } else if (this.abilities[z].title === 'Thrusters' && this.abilities[z].owner === 'Sgt. Hammer') {
+              this.abilities[z].icon = '../../assets/icons/abilities/hammer/siege-thrusters.png';
             } else if (this.abilities[z].title === 'Thrusters') {
               this.abilities[z].icon = '../../assets/icons/abilities/tychus/odin/tychus-odin-thrusters.png';
             }
